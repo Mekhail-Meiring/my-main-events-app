7968a5cdf6662150b728b98a4a7269853dc6799b
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2022-12-17 00:00:11 +0200

    do not need client name anymore

--- common/src/main/java/za/co/simpleChat/models/Client.java
+++ common/src/main/java/za/co/simpleChat/models/Client.java
@@ -5,13 +5,7 @@ package za.co.simpleChat.models;
  * POJO used to parse to and from Json.
  */
 public class Client {
-
-    private String name;
     private String email;
-
-    public String getName() {
-        return name;
-    }
 
     public String getEmail() {
         return email;
